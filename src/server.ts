3f335fdca02d816116e453f6988621a2419e9792
Valdery Alves <valdery.jur@gmail.com>
2023-04-19 15:42:09 -0300

    feat: uses port from env
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { app } from "./app";
 
 dotenv.config();
 
-app.listen(3333, () => {
-  console.log("Server is running");
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
