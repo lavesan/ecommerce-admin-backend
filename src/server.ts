29f9959a4dee964192fe1ebae5131707aa0db4c5
Valdery Alves <valdery.jur@gmail.com>
2023-04-30 19:22:22 -0300

    feat: adds default port
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
