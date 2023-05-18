656ce49c0a31f30425f4913a9fe2938fce5929f8
Valdery Alves <valdery.jur@gmail.com>
2023-05-18 14:56:28 -0300

    feat: adds client authenticated to create new order
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,15 +2,16 @@ import { Router } from "express";
 
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 import { OrderController } from "@modules/order/controller/OrderController";
+import { ensureClientAuthenticated } from "@shared/infra/http/middlewares/ensureClientAuthenticated";
 
 const orderController = new OrderController();
 
 const orderRouter = Router();
 
+orderRouter.post("/", ensureClientAuthenticated, orderController.create);
+
 orderRouter.get("/", ensureAuthenticated, orderController.paginate);
 orderRouter.get("/:id", ensureAuthenticated, orderController.findById);
 orderRouter.patch("/:id", ensureAuthenticated, orderController.update);
-
-orderRouter.post("/", orderController.create);
 
 export { orderRouter };
